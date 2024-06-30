22551ed6671d750624b74cb92806679471bfa443
Jonathan C <johnnymilkshakes@icloud.com>
2024-06-29 23:09:17 -0400

    Refactor to make more DRY and minor improvements

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const numsAndOps = []
 /*-------------------------------- Variables --------------------------------*/
-let numsAndOps = []
-
 let num1 = "",
   num2 = "",
   op = "",
@@ -15,27 +13,22 @@ const display = document.querySelector(".display");
 
 /*----------------------------- Event Listeners -----------------------------*/
 calculator.addEventListener("click", (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  // console.log(event);
 
-  if (event.target.innerText === "C") {
-        display.innerText = "";
-        num1 = "";
-        num2 = "";
-        numsAndOps = []
+    // if user clicks C, clear the display and data in memory
+    if (event.target.innerText === "C") {
+        updateDisplay("")
+        resetData()
 
-  } else if (event.target.classList.contains("number")) {
+        // Logic for when user clicks a number
+    } else if (event.target.classList.contains("number")) {
 
         if (display.innerText === "Error") {
-            display.innerText = "";
+            clearText()
         }
 
         if (equalsPressed === true) {
-            display.innerText = "";
-            num1 = "";
-            num2 = "";
-            numsAndOps = []
+            clearText()
+            resetData()
             equalsPressed = false
         }
 
@@ -44,43 +37,46 @@ calculator.addEventListener("click", (event) => {
             display.innerText === '*' || 
             display.innerText === '/') {
                 numsAndOps.push(display.innerText)
-                display.innerText = "";
+                clearText()
 
         }
 
-        // Do something with a typedNumber
         typedNumber = event.target.innerText;
-        display.innerText = display.innerText + typedNumber;
+        updateDisplay(display.innerText + typedNumber)
 
-  } else if (event.target.classList.contains("operator")) {
+
+        // Logic for when user clicks an operator 
+    } else if (event.target.classList.contains("operator")) {
+
+        console.log(typeof display.innerText)
+
         if (!display.innerText) {
-            display.innerText = "Error";
+            updateDisplay("Error")
+        } else {
+
+            numsAndOps.push(display.innerText)
+
+            clearText();
+    
+            op = event.target.innerText;
+            updateDisplay(display.innerText + op)
         }
 
-        numsAndOps.push(display.innerText)
+        
 
-        display.innerText = ''
-
-        op = event.target.innerText;
-        display.innerText = display.innerText + op;
-
-  } else if (event.target.classList.contains("equals")) {
+        // Logic for when user clicks equals sign
+    } else if (event.target.classList.contains("equals")) {
         equalsPressed = true
 
         numsAndOps.push(display.innerText)
-
-        console.log(numsAndOps)
-        basicCalculator(numsAndOps[0], numsAndOps[2], numsAndOps[1])
 
         num1 = +numsAndOps[0] // unary plus operators to convert string to number, 
         num2 = +numsAndOps[2] // i asked chatgpt how to make that conversion 
         op = numsAndOps[1] 
 
-        console.log(num1, op, num2)
-
-        display.innerText = basicCalculator(num1, num2, op)
-        numsAndOps = []
-  }
+        updateDisplay(basicCalculator(num1, num2, op))
+        numsAndOps.length = 0
+    }
 });
 
 /*-------------------------------- Functions --------------------------------*/
@@ -101,4 +97,18 @@ function basicCalculator(num1, num2, operation) {
     if (operation === "/") {
         return num1 / num2
     }
+}
+
+function clearText() {
+    display.innerText = ''
+}
+
+function resetData() {
+    num1 = "";
+    num2 = "";
+    numsAndOps.length = 0
+}
+
+function updateDisplay(info) {
+    display.innerText = info
 }
