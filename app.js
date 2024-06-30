9a4740e4006a1cac23a6fe9f5a1aef7a4355b2cd
Jonathan C <johnnymilkshakes@icloud.com>
2024-06-29 20:37:12 -0400

    Implement calculator logic and more display logic

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
+let numsAndOps = []
+
 let num1 = "",
   num2 = "",
   op = "",
   typedNumber = "";
-
-let fullInput = "";
 
 /*------------------------ Cached Element References ------------------------*/
 const calculator = document.querySelector("#calculator");
@@ -22,40 +22,72 @@ calculator.addEventListener("click", (event) => {
         display.innerText = "";
         num1 = "";
         num2 = "";
+        numsAndOps = []
+
   } else if (event.target.classList.contains("number")) {
+
         if (display.innerText === "Error") {
             display.innerText = "";
         }
 
-        if (display.innerText === '+' || display.innerText === '-' || display.innerText === '*'|| 
+        if (display.innerText === '+' || 
+            display.innerText === '-' || 
+            display.innerText === '*' || 
             display.innerText === '/') {
-            display.innerText = "";
+                numsAndOps.push(display.innerText)
+                display.innerText = "";
+
         }
 
         // Do something with a typedNumber
         typedNumber = event.target.innerText;
         display.innerText = display.innerText + typedNumber;
 
-        fullInput = fullInput + typedNumber;
-
   } else if (event.target.classList.contains("operator")) {
         if (!display.innerText) {
             display.innerText = "Error";
         }
+
+        numsAndOps.push(display.innerText)
 
         display.innerText = ''
 
         op = event.target.innerText;
         display.innerText = display.innerText + op;
 
-        fullInput = fullInput + op;
-
-        // console.log(fullInput);
   } else if (event.target.classList.contains("equals")) {
-        console.log(fullInput)
-        display.innerText = ''
-        fullInput = ''
+        numsAndOps.push(display.innerText)
+
+        console.log(numsAndOps)
+        basicCalculator(numsAndOps[0], numsAndOps[2], numsAndOps[1])
+
+        num1 = +numsAndOps[0] // unary plus operators to convert string to number, 
+        num2 = +numsAndOps[2] // i asked chatgpt how to make that conversion 
+        op = numsAndOps[1] 
+
+        console.log(num1, op, num2)
+
+        display.innerText = basicCalculator(num1, num2, op)
+        numsAndOps = []
   }
 });
 
 /*-------------------------------- Functions --------------------------------*/
+
+function basicCalculator(num1, num2, operation) {
+    if (operation === "+") {
+        return num1 + num2
+    } 
+    
+    if (operation === "-") {
+        return num1 - num2
+    } 
+    
+    if (operation === "*") {
+        return num1 * num2
+    } 
+    
+    if (operation === "/") {
+        return num1 / num2
+    }
+}
